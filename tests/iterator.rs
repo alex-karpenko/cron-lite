ee4da894f8c3a62323e8b5c9d681b5c7bb994787
Oleksii Karpenko <alexkarpenko@yahoo.com>
2024-12-10 21:29:40 +0200

    chore: Add basic unit tests for Display trait implementations (#3)

--- tests/iterator.rs
+++ tests/iterator.rs
@@ -2,12 +2,23 @@ use chrono::Utc;
 use cron_lite::{Result, Schedule};
 
 #[test]
-fn iterator() -> Result<()> {
+fn iter() -> Result<()> {
     let schedule = Schedule::new("0 0 0 * * *")?;
     let now = Utc::now();
 
     // Get the next 10 timestamps starting from now
     schedule.iter(&now).take(10).for_each(|t| println!("next: {t}"));
+
+    Ok(())
+}
+
+#[test]
+fn into_iter() -> Result<()> {
+    let schedule = Schedule::new("0 0 0 * * *")?;
+    let now = Utc::now();
+
+    // Get the next 10 timestamps starting from now
+    schedule.into_iter(&now).take(10).for_each(|t| println!("next: {t}"));
 
     Ok(())
 }
